30b04b845ba11fab5aa6dffd66f398930f597a46
160JL <j160l@hotmail.es>
2026-05-14 14:29:43 +0200

    Implementacion de tiradas de dados en pantalla de combate, acceso a pantalla de detalles, cambio color de tema

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
       ),
       // Mapeo de rutas de la aplicación a sus respectivos componentes/pantallas.
       routes: {
